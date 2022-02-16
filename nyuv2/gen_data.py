fixed the depth and semantic segmentation rotation

nyuv2/gen_data.py
@@ -50,16 +50,23 @@ if __name__ == "__main__":
         color_image = Image.fromarray(cur_color_map, mode='RGB')
         color_image = rotate_image(color_image)
 
-        depth_image = depth_maps[idx].T
-        #plt.imshow(cur_depth_map.T)
+
+        #print(depth_maps[idx].shape)
+
+        #plt.imshow(color_image)
+        #plt.show()
+        depth_image = np.rot90(depth_maps[idx],3)
+        #print(depth_image.shape)
+        #plt.imshow(depth_image)
         #plt.show()
 
-        label_image = label_maps[idx].T
+        label_image = np.rot90(label_maps[idx],3)
         h, w = label_image.shape
-        #print(f"h: {h}   w: {w}")
         for i in range(h):
             for j in range(w):
+                #print(f"org: {i},{j}:   {label_image[i][j]}")
                 label_image[i][j] = tbl_40_cls[label_image[i][j]]
+                #print(f"                {label_image[i][j]}")
 
         #label_image = Image.fromarray(cur_label_map, mode='I;16')
         #label_image = rotate_image(label_image)
@@ -67,7 +74,7 @@ if __name__ == "__main__":
         
         color_image.save(f"train/images/train_img_{num}.png")
         np.save(f"train/label_depth/depth_{num}.npy", depth_image)
-        depth_image = np.load(f"train/label_depth/depth_{num}.npy")
+        #depth_image = np.load(f"train/label_depth/depth_{num}.npy")
 
         #plt.imshow(depth_image)
         #plt.show()
@@ -104,7 +111,7 @@ if __name__ == "__main__":
         
         color_image.save(f"val/images/train_img_{num}.png")
         np.save(f"val/label_depth/depth_{num}.npy", depth_image)
-        depth_image = np.load(f"val/label_depth/depth_{num}.npy")
+        #depth_image = np.load(f"val/label_depth/depth_{num}.npy")
 
         #plt.imshow(depth_image)
         #plt.show()

nyuv2/nyu.py
@@ -2,9 +2,12 @@ import numpy as np
 from PIL import Image
 import os, torch
 from torchvision.transforms import transforms
+from torchvision.utils import draw_segmentation_masks
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+
+torch.set_printoptions(profile="full")
 
 class NyuV2Dataset:
     """Python interface for the labeled subset of the NYU dataset.
@@ -59,13 +62,31 @@ class NyuV2Dataset:
 ###########################
 if __name__ == '__main__':
     transform_train = transforms.Compose([transforms.ToTensor()])
-    nyu_v2_train = NyuV2Dataset("/home/hhwu/project/semantic_seg_and_depth/nyuv2/train/", "/home/hhwu/project/semantic_seg_and_depth/nyuv2/label_depth/", "/home/hhwu/project/semantic_seg_and_depth/nyuv2/label_semantic_seg/",transform=transform_train)
+    nyu_v2_train = NyuV2Dataset("/home/hhwu/project/semantic_seg_and_depth/nyuv2/train/images/", "/home/hhwu/project/semantic_seg_and_depth/nyuv2/train/label_depth/", "/home/hhwu/project/semantic_seg_and_depth/nyuv2/train/label_semantic_seg/",transform=transform_train)
 
     nyu_v2_train_sampler = torch.utils.data.RandomSampler(nyu_v2_train)
     nyu_v2_train_dataloader = DataLoader(nyu_v2_train, batch_size=1, shuffle=False, sampler=nyu_v2_train_sampler, num_workers=0)
+    #nyu_v2_train_dataloader = DataLoader(nyu_v2_train, batch_size=1, shuffle=False, num_workers=0)
 
     nyu_v2_progress_bar = tqdm(nyu_v2_train_dataloader)
     nyu_v2_itr = iter(nyu_v2_progress_bar)
 
-    img,label = next(nyu_v2_itr)
+    image,label = next(nyu_v2_itr)
 
+    img = image[0].permute(1,2,0)
+
+    plt.imshow(img)
+    plt.show()
+
+
+    t_img = image[0]
+    t_img = t_img*255
+    for i in range(1,41):
+        bool_mask = label[0] == i
+#        print(bool_mask.shape)
+
+
+        t_img = draw_segmentation_masks(t_img.type(torch.uint8), masks=bool_mask,alpha=0.7)
+
+        plt.imshow(t_img.permute(1,2,0))
+        plt.show()

test.py
@@ -18,7 +18,7 @@ import argparse, os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Evaluate the FCN semantic segmentation model with NYU depth V2')
     parser.add_argument('--testBatchSize', type=int, default=8, help='testing batch size')
-    parser.add_argument('--data_path', type=str, default='/home/hhwu/project/semantic_seg_and_depth/nyuv2/', help="path to the data folder")
+    parser.add_argument('--data_path', type=str, default='/home/hhwu/project/semantic_seg_and_depth/nyuv2/val/', help="path to the data folder")
     parser.add_argument('--dataset', type=int, default=0, help='0: semantic segmentation, 1: depth estimation')
     parser.add_argument('--model_path', type=str, default='/home/hhwu/project/semantic_seg_and_depth/saved_model/ep010.pth', help="path to the saved model folder")
     
@@ -26,14 +26,15 @@ if __name__ == "__main__":
     print(opt)
 
 
-    opt.train_data_path               = opt.data_path + 'train/'
-    opt.train_label_semantic_seg_path = opt.data_path + 'label_semantic_seg/'
-    opt.train_label_depth_path        = opt.data_path + 'label_depth/'
+    opt.val_data_path               = opt.data_path + 'images/'
+    opt.val_label_semantic_seg_path = opt.data_path + 'label_semantic_seg/'
+    opt.val_label_depth_path        = opt.data_path + 'label_depth/'
 
 
     transform_train = transforms.Compose([transforms.ToTensor()])
-    nyu_v2_train = NyuV2Dataset(opt.train_data_path, opt.train_label_depth_path, opt.train_label_semantic_seg_path,transform=transform_train)
-    nyu_v2_val_dataloader = DataLoader(nyu_v2_train, batch_size=8, shuffle=False, num_workers=8)
+    #transform_train = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    nyu_v2_val = NyuV2Dataset(opt.val_data_path, opt.val_label_depth_path, opt.val_label_semantic_seg_path,transform=transform_train)
+    nyu_v2_val_dataloader = DataLoader(nyu_v2_val, batch_size=8, shuffle=False, num_workers=8)
 
     distributed = False
 
@@ -43,6 +44,8 @@ if __name__ == "__main__":
     model = torchvision.models.segmentation.fcn_resnet50(pretrained=False, num_classes=41).to(device)
     model.load_state_dict(torch.load(opt.model_path)['model'])
 
+    
+    model.eval()
     eval_res = JaccardIndex(num_classes=41, ignore_index=0, compute_on_step=False).cuda()
     with torch.no_grad():
         for img, target in tqdm(nyu_v2_val_dataloader):
@@ -50,7 +53,12 @@ if __name__ == "__main__":
             target = target.cuda()
             pred = model(img)['out']
 
-            eval_res.update(pred,target)
+            
+            #print(f"pred: {pred.shape}  {type(pred)}")
+            #print(f"    {pred[0,:,0,0]}")
+            #print(f"target: {target.shape}    {type(target)}")
+            #print(f"    {target[0][0][0]}")
+            eval_res(pred,target)
             
         print(eval_res.compute())
 

train.py
@@ -36,13 +36,13 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = torchvision.models.segmentation.fcn_resnet50(pretrained=False, num_classes=41).to(device)
     #model = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=False, num_classes=41).to(device)
-    print(model)
+    #print(model)
 
     
 
     params = [p for p in model.parameters() if p.requires_grad]
     learning_rate = 1e-3
-    semantic_seg_optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=0.001)
+    semantic_seg_optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=0.0001)
     #semantic_seg_optimizer = torch.optim.Adam(params, lr=learning_rate, weight_decay=0.0001)
 
 
@@ -53,8 +53,6 @@ if __name__ == "__main__":
     criterion = torch.nn.CrossEntropyLoss(ignore_index=0) # Set loss function
 
     seg_ave_loss = 0.0
-    h=480
-    w=640
     for epoch in range(101):
         depth_data_available = True
         t_data_0 = time.time()